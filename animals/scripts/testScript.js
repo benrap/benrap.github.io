f572ad70640ec3b5b040847a4e6a17a63e5e6900
benrap <13122904+benrap@users.noreply.github.com>
2018-09-15 01:09:08 +0300

    Create testScript.js

---
+++ animals/scripts/testScript.js
@@ -0,0 +1,72 @@
+var validNames = ["anner", "ben", "gal", "itay", "omri", "yakir"]
+
+window.onload = choosePic;
+
+function checkIfAnimal() {
+     var path = window.location.pathname;
+     var page = path.split("/").pop();
+     var animalName;
+     
+     if(path.substring(0,path.lastIndexOf("/")) == "benrap.github.io/animals") {
+           if(page.lastIndexOf(".html")==page.length-5 && page.length != 4){
+                animalName = page.substring(0, page.length - 5);
+           } else {
+                animalName = page;
+           }
+
+           if(validNames.includes(animalName)) {
+              RunAnimal();
+           } else {
+              RunNormal();
+           }
+     } else {
+          RunNormal();
+     }
+}
+
+function RunNormal() {
+    var elem = document.getElementById('animalImage');
+    elem.parentNode.removeChild(elem);
+}
+
+function RunAnimal() {
+    var elem = document.getElementById('actual404');
+    elem.parentNode.removeChild(elem);
+    choosePic();
+}
+
+function choosePic() {
+     var fileList;
+     var path = window.location.pathname;
+     var page = path.split("/").pop();
+     var name;
+     if(page.lastIndexOf(".html")==page.length-5 && page.length != 4){
+          name = page.substring(0, page.length - 5);
+     } else {
+          name = page;
+     }
+     
+     switch(name) {
+          case "gal":
+               fileList = ["0.jpeg", "1.jpeg"];
+               break;
+          case "anner":
+               fileList = ["0.png"];
+               break;
+          case "omri":
+               fileList = ["0.png"];
+               break;
+          case "itay":
+               fileList = ["0.png"];
+               break;
+          case "ben":
+               fileList = ["0.png"];
+               break;
+          case "yakir":
+               fileList = ["0.png"];
+               break;
+     }
+     
+     var randomNum = Math.floor(Math.random() * fileList.length);
+     document.getElementById("pic").src = "pictures/" + name + "/" + fileList[randomNum];
+}
