eb878a3c75ea50cf918bc9326aecc22345719773
benrap <13122904+benrap@users.noreply.github.com>
2018-09-14 22:20:55 +0300

    Update gal.js

--- animals/scripts/gal.js
+++ animals/scripts/gal.js
@@ -1,7 +1,7 @@
 window.onload = choosePic;
 
-var myPix = new Array("../images/gal_0.jpeg","../images/gal_1.jpeg");
+var myPix = new Array("../pictures/gal/0.jpeg","../pictures/gal/1.jpeg");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
-     document.getElementById("myPicture").src = myPix[randomNum];
+     document.getElementById("pic").src = myPix[randomNum];
