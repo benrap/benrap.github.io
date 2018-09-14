121c4d1eca1e8c454153793b99029f3a7d04c298
benrap <13122904+benrap@users.noreply.github.com>
2018-09-14 22:34:24 +0300

    Update gal.js

--- animals/scripts/gal.js
+++ animals/scripts/gal.js
@@ -1,6 +1,10 @@
 window.onload = choosePic;
 
-var myPix = new Array("pictures/gal/0.jpeg","pictures/gal/1.jpeg");
+var path = window.location.pathname;
+var page = path.split("/").pop();
+var name = page.substring(0, page.length - 5);
+
+var myPix = new Array("pictures/" + name + "/0.jpeg","pictures/" + name + "/1.jpeg");
 
 function choosePic() {
      var randomNum = Math.floor(Math.random() * myPix.length);
